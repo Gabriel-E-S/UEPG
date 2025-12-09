e744b223630a3a8ffd0955b92db97e89a4fffbf0
Gabriel do Espírito Santo <gabriel.e.santo007@gmail.com>
2025-12-09 20:01:10 -0300

    Inserindo os códigos
    Colocando os códigos do aplicativo em java que testa e compara os dois diferentes algoritmos de desenho de linhas na tela.

---
+++ Computação Gráfica/Desenhando Linhas/README.md
@@ -0,0 +1,8 @@
+# Como desenhar uma linha?
+
+Por mais que pareça ser algo simples, na verdade existem muitos algoritmos que foram criados para esse propósito.
+
+Nesse repositório está a implementação de um código onde são comparados o algoritmo de reta linear e o algoritmo de Bresenham.
+
+Toda a implementação foi feita em Java. Bons estudos!
+
---
+++ Computação Gráfica/Desenhando Linhas/espiritosanto/Pixel.java
@@ -0,0 +1,24 @@
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Espírito Santo
+ */
+public class Pixel {
+    public int x;
+    public int y;
+    public Color cor;
+    
+    public Pixel(int x, int y, Color cor){
+        this.x = x;
+        this.y = y;
+        this.cor = cor;
+    }
+}
