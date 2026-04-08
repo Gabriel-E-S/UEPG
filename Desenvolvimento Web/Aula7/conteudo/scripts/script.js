7ed2bfc169e05d880a8c070c8de4a5a0f54b4037
Gabriel do Espírito Santo <gabriel.e.santo007@gmail.com>
2026-04-08 09:34:02 -0300

    feat: add Aula7 e conteudo
    Vendo o começo de javascript

---
+++ Desenvolvimento Web/Aula7/conteudo/scripts/script.js
@@ -0,0 +1,34 @@
+console.log("Hello World!");
+
+alert("Welcome to my website!");
+
+let nome = "Gabriel";
+
+const anoAtual = 2026;
+
+let anoNascimento = 2005;
+
+let idade = anoAtual - anoNascimento;
+
+console.log("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.");
+
+alert("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.");
+
+if(true == true || idade  >= 18){
+    console.log("A condição é verdadeira.");
+}
+
+function soma(a, b){
+    return a + b ;
+}
+
+function alerta(){
+    alert("Você clicou no botão");
+    confirm("Confirma ai")
+    prompt("Qual seu nome?");
+}
+
+a = soma(1,2)
+
+console.log(a);
+
